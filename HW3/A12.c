b26002933bcc3b240a91b910f64ec0663722c233
KVV <gerundic47@gmail.com>
2024-11-04 15:37:16 +0400

    Contest A HW3 	modified:   HW3/A12.c 	modified:   HW3/A13.c

--- HW3/A12.c
+++ HW3/A12.c
@@ -8,9 +8,10 @@ int main(int argc, char **argv)
 	scanf("%d",&a);
 	int sum=0;
 	for(int i=0;i<3;i++)
-		{sum+=a%10;
-		a/=10;}
-
+	{
+		sum+=a%10;
+		a/=10;
+	}
 	printf("%d",sum);
 	return 0;
 }
--- HW3/A13.c
+++ HW3/A13.c
@@ -6,9 +6,10 @@ int main(int argc, char **argv)
 	scanf("%d",&a);
 	int sum=1;
 	for(int i=0;i<3;i++)
-		{sum*=a%10;
-		a/=10;}
-
+	{
+		sum*=a%10;
+		a/=10;
+	}
 	printf("%d",sum);
 	return 0;
 }
